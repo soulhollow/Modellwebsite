78c423ac70d030ab3a83cfecaa1f93700e14c694
Florian Ruffner <fr@ruffner-beratung.de>
2024-09-06 11:48:44 +0200

    test inital commit

---
+++ ModellBackend/src/test/java/com/example/modellbackend/ModellBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modellbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModellBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
